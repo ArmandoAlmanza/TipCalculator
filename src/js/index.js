91f00fe1e3dd91b9df293b058ab6edfa8207125e
Armando Almanza <armando.s.almanza@gmail.com>
2021-08-27 07:18:17 -0500

    finishing with the js

--- src/js/index.js
+++ src/js/index.js
@@ -25,29 +25,56 @@ const tip = () => {
     });
     document.getElementById("custom__tip").addEventListener("input", (e) => {
         porcetange = Number(e.target.value);
+        if (e.target.value === "") {
+            document.getElementById("custom__tip").classList.add("error");
+            document.getElementById("custom__tip").classList.remove("correct");
+            document.getElementById("alert__custom").classList.add("d-block");
+            document.getElementById("alert__custom").classList.remove("d-none");
+        } else {
+            document.getElementById("custom__tip").classList.remove("error");
+            document.getElementById("custom__tip").classList.add("correct");
+            document
+                .getElementById("alert__custom")
+                .classList.remove("d-block");
+            document.getElementById("alert__custom").classList.add("d-none");
+            dollars = Number(e.target.value.trim());
+        }
     });
 };
 
 const bill = () => {
     document.getElementById("bill").addEventListener("input", (e) => {
         if (e.target.value === "") {
-            document.getElementById('bill').classList.add('error');
-            document.getElementById('bill').classList.remove('correct');
+            document.getElementById("bill").classList.add("error");
+            document.getElementById("bill").classList.remove("correct");
+            document.getElementById("alert__bill").classList.add("d-block");
+            document.getElementById("alert__bill").classList.remove("d-none");
         } else {
-            document.getElementById('bill').classList.remove('error');
-            document.getElementById('bill').classList.add('correct');
+            document.getElementById("bill").classList.remove("error");
+            document.getElementById("bill").classList.add("correct");
+            document.getElementById("alert__bill").classList.remove("d-block");
+            document.getElementById("alert__bill").classList.add("d-none");
             dollars = Number(e.target.value.trim());
         }
+    });
+    document.getElementById("bill").addEventListener("blur", (e) => {
+        result();
     });
 };
 const people = () => {
     document.getElementById("people").addEventListener("input", (e) => {
         if (e.target.value === "") {
-            document.getElementById('people').classList.add('error');
-            document.getElementById('people').classList.remove('correct');
+            document.getElementById("people").classList.add("error");
+            document.getElementById("people").classList.remove("correct");
+            document.getElementById("alert__people").classList.add("d-block");
+            document.getElementById("alert__people").classList.remove("d-none");
         } else {
-            document.getElementById('people').classList.remove('error');
-            document.getElementById('people').classList.add('correct');
+            document.getElementById("people").classList.remove("error");
+            document.getElementById("people").classList.add("correct");
+            document
+                .getElementById("alert__people")
+                .classList.remove("d-block");
+            document.getElementById("alert__people").classList.add("d-none");
             number_people = Number(e.target.value.trim());
         }
     });
